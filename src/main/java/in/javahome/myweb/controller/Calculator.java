224d58408ca631a2aed8fac34916ddf43091ee48
srikanthambati999 <49298862+srikanthambati999@users.noreply.github.com>
2019-12-04 13:43:10 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -3,31 +3,14 @@ package in.javahome.myweb.controller;
  * 
  */
 public class Calculator {
-	
-	String ptr = null; 
-  
-        // Checking if ptr.equals null or works fine. 
-        try
-        { 
-            // This line of code throws NullPointerException 
-            // because ptr is null 
-            if (ptr.equals("gfg")) 
-                System.out.print("Same"); 
-            else 
-                System.out.print("Not Same"); 
-        } 
-        catch(NullPointerException e) 
-        { 
-            System.out.print("NullPointerException Caught"); 
-        } 
-	
 	/*
 	 * @param i
 	 * @param j
 	 * @return int
 	 */
 	public int add(int i, int j){
-		
+		string s;
+		string s;
 		return i+j;
 		
 	}
